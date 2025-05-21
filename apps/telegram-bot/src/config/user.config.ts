7bbf4cdf71d2dd60913d64bbb6d6f58a6a2216cc
iren-pd <irenpd45@gmail.com>
2025-05-21 23:06:10 +0300

    Edit constants for allowed users

--- apps/telegram-bot/src/config/user.config.ts
+++ apps/telegram-bot/src/config/user.config.ts
@@ -1,4 +1,4 @@
 export const allowedUsers = [
-  process.env.ROOTISALIE_ADMIN_NICKNAME,
-  process.env.BULKA_ADMIN_NICKNAME,
+  process.env.NX_ROOTISALIE_ADMIN_NICKNAME,
+  process.env.NX_BULKA_ADMIN_NICKNAME,
 ];
