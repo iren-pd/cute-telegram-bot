a581bba02b6c67693da3271bf17632bd840ce4b2
iren-pd <irenpd45@gmail.com>
2025-05-30 12:37:29 +0300

    fix: keyboard for dishes

--- apps/telegram-bot/src/bot/keyboards/user/dishKeyboard.ts
+++ apps/telegram-bot/src/bot/keyboards/user/dishKeyboard.ts
@@ -1,30 +1,22 @@
+import { Markup } from 'telegraf';
 import { Dish } from '../../../models/dish.model';
 import { chunkArray } from '../../../utils/chunkArray';
 
 export function getDishKeyboard(dish: Dish) {
-  const keyboard = {
-    inline_keyboard: [] as any[],
-  };
-
-  if (dish.options && dish.options.length > 0) {
-    const chunkedOptions = chunkArray(dish.options, 2);
-    keyboard.inline_keyboard.push(
-      ...chunkedOptions.map((row) =>
-        row.map((option) => ({
-          text: option,
-          callback_data: `option_${option}`,
-        }))
-      )
-    );
-  }
-
-  keyboard.inline_keyboard.push(
-    [{ text: '🛒 Добавить в заказ', callback_data: `add_to_cart_${dish.id}` }],
-    [
-      { text: '⬅️ Назад', callback_data: 'back' },
-      { text: '🛒 Корзина', callback_data: 'cart' },
-    ]
+  const optionButtons = (dish.options || []).map((option, idx) =>
+    Markup.button.callback(option, `option_${idx}`)
   );
 
-  return keyboard;
+  const keyboard = chunkArray(optionButtons, 2);
+
+  keyboard.push([
+    Markup.button.callback('🛒 Добавить в заказ', `add_to_cart_${dish.id}`),
+  ]);
+
+  keyboard.push([
+    Markup.button.callback('⬅️ Назад', 'back'),
+    Markup.button.callback('🛒 Корзина', 'cart'),
+  ]);
+
+  return Markup.inlineKeyboard(keyboard);
 }
