18fdb446a0a90e96d70384de11ae8676453f40e2
iren-pd <irenpd45@gmail.com>
2025-05-30 10:57:43 +0300

    feat: add dessert to all dishes

--- apps/telegram-bot/src/data/dishes/index.ts
+++ apps/telegram-bot/src/data/dishes/index.ts
@@ -2,5 +2,12 @@ import { dishesBreakfast } from './dishesBreakfast';
 import { dishesLunch } from './dishesLunch';
 import { dishesSalad } from './dishesSalad';
 import { dishesBeerDish } from './dishesBeerDish';
+import { dishesDessert } from './dishesDessert';
 
-export const dishes = [...dishesBreakfast, ...dishesLunch, ...dishesSalad, ...dishesBeerDish];
+export const dishes = [
+  ...dishesBreakfast,
+  ...dishesLunch,
+  ...dishesSalad,
+  ...dishesDessert,
+  ...dishesBeerDish,
+];
